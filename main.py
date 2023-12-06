0f9ccc2e2fbcb9bf8f9990a2ff3f26c897a5f8a8
Chigozie Nri <chigozie@replicate.com>
2023-12-06 11:32:12 +0000

    Black + isort

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-from detectron2.config import get_cfg
-import torch
+import argparse
+
 import cv2
 import numpy as np
-from detectron2.engine import DefaultPredictor
+import torch
 from densepose import add_densepose_config
+from densepose.vis.densepose_results import (
+    DensePoseResultsFineSegmentationVisualizer as Visualizer,
+)
 from densepose.vis.extractor import DensePoseResultExtractor
-from densepose.vis.densepose_results import DensePoseResultsFineSegmentationVisualizer as Visualizer
-import argparse
+
+from detectron2.config import get_cfg
+from detectron2.engine import DefaultPredictor
+
 
 def main(input_video_path="./input_video.mp4", output_video_path="./output_video.mp4"):
     # Initialize Detectron2 configuration for DensePose
     cfg = get_cfg()
     add_densepose_config(cfg)
-    cfg.merge_from_file("detectron2/projects/DensePose/configs/densepose_rcnn_R_50_FPN_s1x.yaml")
+    cfg.merge_from_file(
+        "detectron2/projects/DensePose/configs/densepose_rcnn_R_50_FPN_s1x.yaml"
+    )
     cfg.MODEL.WEIGHTS = "https://dl.fbaipublicfiles.com/densepose/densepose_rcnn_R_50_FPN_s1x/165712039/model_final_162be9.pkl"
     predictor = DefaultPredictor(cfg)
 
@@ -23,7 +30,7 @@ def main(input_video_path="./input_video.mp4", output_video_path="./output_video
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
     # Initialize video writer
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
     out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
 
     # Process each frame
@@ -33,26 +40,31 @@ def main(input_video_path="./input_video.mp4", output_video_path="./output_video
             break
 
         with torch.no_grad():
-            outputs = predictor(frame)['instances']
-        
+            outputs = predictor(frame)["instances"]
+
         results = DensePoseResultExtractor()(outputs)
 
         # MagicAnimate uses the Viridis colormap for their training data
-        cmap=cv2.COLORMAP_VIRIDIS
+        cmap = cv2.COLORMAP_VIRIDIS
         # Visualizer outputs black for background, but we want the 0 value of
         # the colormap, so we initialize the array with that value
         arr = cv2.applyColorMap(np.zeros((height, width), dtype=np.uint8), cmap)
-        out_frame = Visualizer(alpha=0.7,cmap=cmap).visualize(arr, results)
+        out_frame = Visualizer(alpha=0.7, cmap=cmap).visualize(arr, results)
         out.write(out_frame)
 
     # Release resources
     cap.release()
     out.release()
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input_video_path", type=str, default="./input_video.mp4")
-    parser.add_argument("-o", "--output_video_path", type=str, default="./output_video.mp4")
+    parser.add_argument(
+        "-i", "--input_video_path", type=str, default="./input_video.mp4"
+    )
+    parser.add_argument(
+        "-o", "--output_video_path", type=str, default="./output_video.mp4"
+    )
     args = parser.parse_args()
 
     main(args.input_video_path, args.output_video_path)
